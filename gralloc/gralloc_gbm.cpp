625e814107d60b08f53e1fb2b6a700e6ae7d4602
pngcui <pngcui1224@163.com>
2026-04-09 14:08:15 +0800

    support d3000m yv12

--- gralloc/gralloc_gbm.cpp
+++ gralloc/gralloc_gbm.cpp
@@ -51,6 +51,7 @@
 #include <cmath>
 
 int flag_is_mesa_env = 0;
+int flag_is_proxy_env = 0;
 #define GRALLOC_ALIGN(value, base) (((value) + ((base)-1)) & ~((base)-1))
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
@@ -136,7 +137,7 @@ static uint32_t get_gbm_format(int format)
 			fmt = GBM_FORMAT_ARGB8888;
 		break;
 	case HAL_PIXEL_FORMAT_YV12:
-		if (flag_is_mesa_env) {
+		if (flag_is_mesa_env || flag_is_proxy_env) {
 			fmt = GBM_FORMAT_RGB565;
 		} else {
 			/* YV12 is planar, but must be a single buffer so ask for GR88 */
@@ -261,8 +262,13 @@ static struct gbm_bo *gbm_import(struct gbm_device *gbm,
 	if (handle->format == HAL_PIXEL_FORMAT_YV12 || handle->format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
 		data.width /= 2;
 		data.height += handle->height / 2;
-	} else if (handle->format == HAL_PIXEL_FORMAT_RGB_565 && (handle->usage & GRALLOC_USAGE_PRIVATE_0)) {
-		data.width = GRALLOC_ALIGN(data.width, 256);
+	}
+	if (handle->usage & GRALLOC_USAGE_PRIVATE_0 && handle->format == HAL_PIXEL_FORMAT_RGB_565) {
+		if (flag_is_mesa_env) {
+			data.width = GRALLOC_ALIGN(data.width, 256);
+		} else if (flag_is_proxy_env) {
+			data.width = GRALLOC_ALIGN(data.width, 32);
+		}
 	}
 	if (handle->format == HAL_PIXEL_FORMAT_BLOB) {
 		std::pair<int, int> size = find_closest_size(data.width);
@@ -280,6 +286,11 @@ static struct gbm_bo *gbm_import(struct gbm_device *gbm,
 	data.stride = handle->stride;
 	bo = gbm_bo_import(gbm, GBM_BO_IMPORT_FD, &data, 0);
 	#endif
+
+	if (!bo) {
+		ALOGE("failed to import BO, size=%dx%d, fmt=%d",
+		      handle->width, handle->height, handle->format);
+	}
 
 	return bo;
 }
@@ -313,10 +324,15 @@ static struct gbm_bo *gbm_alloc(struct gbm_device *gbm,
 		if (format == GBM_FORMAT_RGB565) {
 			handle->covert_format = 1;
 		}
-	} else if (handle->format == HAL_PIXEL_FORMAT_RGB_565 && (handle->usage & GRALLOC_USAGE_PRIVATE_0))  {
-		width = GRALLOC_ALIGN(width, 256);
 	}
-
+	if (handle->usage & GRALLOC_USAGE_PRIVATE_0 && handle->format == HAL_PIXEL_FORMAT_RGB_565) {
+		if (flag_is_mesa_env) {
+			width = GRALLOC_ALIGN(width, 256);
+		} else if (flag_is_proxy_env) {
+			width = GRALLOC_ALIGN(width, 32);
+		}
+	}
+	
 	if (handle->format == HAL_PIXEL_FORMAT_BLOB) {
 		std::pair<int, int> size = find_closest_size(width);
 		width = size.first;
@@ -341,10 +357,13 @@ static struct gbm_bo *gbm_alloc(struct gbm_device *gbm,
 	handle->prime_fd = gbm_bo_get_fd(bo);
 	handle->stride = gbm_bo_get_stride(bo);
 	if ((handle->usage & GRALLOC_USAGE_PRIVATE_0)
-		&& handle->format == HAL_PIXEL_FORMAT_RGB_565 ) {
+			&& handle->format == HAL_PIXEL_FORMAT_RGB_565
+			&& (flag_is_mesa_env)) {
+		ALOGD("pngcui - create BO, size=%dx%d, fmt=%d, usage=%x, stride:%d->%d",
+			handle->width, handle->height, handle->format, usage, handle->stride,
+			(handle->width * gralloc_gbm_get_bpp(handle->format)));
 		handle->stride = handle->width * gralloc_gbm_get_bpp(handle->format);
 	}
-
 	#ifdef GBM_BO_IMPORT_FD_MODIFIER
 	handle->modifier = gbm_bo_get_modifier(bo);
 	#endif
@@ -435,6 +454,8 @@ struct gbm_device *gbm_dev_create(void)
 	property_get("ro.hardware.egl", egl_type, "none");
 	if (strcmp(egl_type, "mesa") == 0) {
 		flag_is_mesa_env = 1;
+	} else if (strcmp(egl_type, "proxy") == 0) {
+		flag_is_proxy_env = 1;
 	}
 
 	return gbm;
@@ -645,7 +666,7 @@ int gralloc_gbm_bo_lock_ycbcr(buffer_handle_t handle,
 int gralloc_gbm_need_covert_format(buffer_handle_t _handle)
 {
 	struct gralloc_handle_t *handle = gralloc_handle(_handle);
-	if (flag_is_mesa_env) {
+	if (flag_is_mesa_env || flag_is_proxy_env) {
 		return handle->covert_format;
 	}
 	return 0;
